cd7eb69696f408850e8aa5c63c3d21ba267f61e3
Javier García González <javier@garciagon.com>
2021-12-09 08:39:47 +0100

    refactor: polish 8b

--- 8/8b.js
+++ 8/8b.js
@@ -3,77 +3,70 @@ const { getDataFromTxtFileForPart2 } = require('./dataGetter.js');
 const getSortedString = (str) =>
   str.split('').sort().join().replaceAll(',', '');
 
-const getDecodedNumbers = (digits) => {
+const uniqueLengthNumbers = {
+  2: 1,
+  3: 7,
+  4: 4,
+  7: 8,
+};
+
+const everyLetterMatches = (stringToMatch, stringToCheck) =>
+  stringToMatch.split('').every((letter) => stringToCheck.includes(letter));
+
+const someLettersMatch = (stringToMatch, stringToCheck, amountOfLetters) =>
+  stringToMatch.split('').filter((letter) => stringToCheck.includes(letter))
+    .length === amountOfLetters;
+
+const getDecodedNumbers = (patterns) => {
   const decodedNumbersTable = {};
 
-  decodedNumbersTable['1'] = digits.find(
-    (digitGroup) => digitGroup.length === 2
-  );
-  decodedNumbersTable['4'] = digits.find(
-    (digitGroup) => digitGroup.length === 4
-  );
-  decodedNumbersTable['7'] = digits.find(
-    (digitGroup) => digitGroup.length === 3
-  );
-  decodedNumbersTable['8'] = digits.find(
-    (digitGroup) => digitGroup.length === 7
-  );
+  // Get unique length patterns
+  Object.keys(uniqueLengthNumbers).forEach((knownLength) => {
+    const pattern = patterns.find(
+      (pattern) => pattern.length === parseInt(knownLength)
+    );
+    decodedNumbersTable[uniqueLengthNumbers[knownLength]] = pattern;
+  });
 
-  decodedNumbersTable['9'] = digits.find(
-    (digitGroup) =>
-      digitGroup.length === 6 &&
-      decodedNumbersTable['4']
-        .split('')
-        .every((letter) => digitGroup.includes(letter))
-  );
-  decodedNumbersTable['0'] = digits.find(
-    (digitGroup) =>
-      digitGroup.length === 6 &&
-      decodedNumbersTable['7']
-        .split('')
-        .every((letter) => digitGroup.includes(letter)) &&
-      digitGroup !== decodedNumbersTable['9']
-  );
+  // Get tricky patterns
+  patterns.forEach((pattern) => {
+    const groupLength = pattern.length;
 
-  decodedNumbersTable['6'] = digits.find(
-    (digitGroup) =>
-      digitGroup.length === 6 &&
-      digitGroup !== decodedNumbersTable['0'] &&
-      digitGroup !== decodedNumbersTable['9']
-  );
+    if (groupLength === 5) {
+      if (everyLetterMatches(decodedNumbersTable['7'], pattern)) {
+        decodedNumbersTable['3'] = pattern;
+      } else if (
+        someLettersMatch(decodedNumbersTable['4'], pattern, 3) &&
+        pattern !== decodedNumbersTable['3']
+      ) {
+        decodedNumbersTable['5'] = pattern;
+      } else {
+        decodedNumbersTable['2'] = pattern;
+      }
+    }
 
-  decodedNumbersTable['3'] = digits.find(
-    (digitGroup) =>
-      digitGroup.length === 5 &&
-      decodedNumbersTable['7']
-        .split('')
-        .every((letter) => digitGroup.includes(letter))
-  );
-
-  decodedNumbersTable['5'] = digits.find(
-    (digitGroup) =>
-      digitGroup.length === 5 &&
-      decodedNumbersTable['4']
-        .split('')
-        .filter((letter) => digitGroup.includes(letter)).length === 3 &&
-      digitGroup !== decodedNumbersTable['3']
-  );
-
-  decodedNumbersTable['2'] = digits.find(
-    (digitGroup) =>
-      digitGroup.length === 5 &&
-      digitGroup !== decodedNumbersTable['5'] &&
-      digitGroup !== decodedNumbersTable['3']
-  );
+    if (groupLength === 6) {
+      if (everyLetterMatches(decodedNumbersTable['4'], pattern)) {
+        decodedNumbersTable['9'] = pattern;
+      } else if (
+        everyLetterMatches(decodedNumbersTable['7'], pattern) &&
+        pattern !== decodedNumbersTable['9']
+      ) {
+        decodedNumbersTable['0'] = pattern;
+      } else {
+        decodedNumbersTable['6'] = pattern;
+      }
+    }
+  });
 
   return decodedNumbersTable;
 };
 
-const getDecodedOutput = (inputDigits, outputDigits) => {
-  const decodedNumbersTable = getDecodedNumbers(inputDigits);
+const getDecodedOutput = (inputPatterns, outputPatterns) => {
+  const decodedNumbersTable = getDecodedNumbers(inputPatterns);
   let finalOutputNumber = '';
 
-  outputDigits.forEach((output) => {
+  outputPatterns.forEach((output) => {
     finalOutputNumber += Object.keys(decodedNumbersTable).find((key) =>
       decodedNumbersTable[key] === output ? key : null
     );
@@ -88,10 +81,10 @@ const getDecodedOutput = (inputDigits, outputDigits) => {
   const result = data.reduce((total, row, index) => {
     const [input, output] = row.split(' | ');
 
-    const inputDigits = input.split(' ').map((str) => getSortedString(str));
-    const outputDigits = output.split(' ').map((str) => getSortedString(str));
+    const inputPatterns = input.split(' ').map((str) => getSortedString(str));
+    const outputPatterns = output.split(' ').map((str) => getSortedString(str));
 
-    const outputt = getDecodedOutput(inputDigits, outputDigits);
+    const outputt = getDecodedOutput(inputPatterns, outputPatterns);
 
     return total + outputt;
   }, 0);
